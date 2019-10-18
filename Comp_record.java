09dd3d67eaaff0c3651a7e631bb1df32515ce2d9
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-10-18 12:28:49 -0600

    Update Comp_record.java

--- Comp_record.java
+++ Comp_record.java
@@ -1,14 +1,3 @@
-package Store;
-import Customer_folder.Customer;
-import Customer_folder.Customer_assemble;
-import Tool_folder.Tool;
-import Tool_folder.Tool_assemble;
-import Option.Option_tool;
-import Store.Store;
-import Option.Optional_assemble;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 public class Comp_record extends Rent_record
 {
   private int completed = 1;
